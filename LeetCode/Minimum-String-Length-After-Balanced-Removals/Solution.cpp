130d7af0d3e000c29aec25f8c5262873b5e5fe1e
Jaswanth <147476570+23A91A0561@users.noreply.github.com>
2026-01-19 21:36:07 +0530

    Add solution for Minimum String Length After Balanced Removals

---
+++ LeetCode/Minimum-String-Length-After-Balanced-Removals/Solution.cpp
@@ -0,0 +1,8 @@
+def minLengthAfterRemovals(self, s: str) -> int:
+        stack = []
+        for i in s:
+            if stack and stack[-1] != i:
+                stack.pop()
+            else:
+                stack.append(i)
+        return len(stack)
