a948aeaf84065af5ac1ae4bc41d41eb60ad3e91f
Jaswanth <23A91A0561@aec.edu.in>
2026-03-14 11:40:20 +0530

    Add solution for Find Users with Persistent Behavior Patterns

--- LeetCode/Find-Users-with-Persistent-Behavior-Patterns/Solution.sql
+++ LeetCode/Find-Users-with-Persistent-Behavior-Patterns/Solution.sql
@@ -1,15 +1,7 @@
 1# Write your MySQL query statement below
-2with cte as(
-3SELECT user_id,action_date,action,date_sub(action_date,INTERVAL 
-4        ROW_NUMBER() OVER(
-5            PARTITION BY user_id, action 
-6            ORDER BY action_date
-7        ) DAy
-8    ) AS rad 
-9from activity
-10)
-11select user_id,action,count(action_date) as streak_length,min(action_date) as start_date,max(action_date) as end_date
-12from cte
-13group by user_id,action,rad
-14having count(action_date)>=5
-15order by streak_length desc
+2select user_id,action,count(action_date) as streak_length,min(action_date) as start_date,max(action_date) as end_date
+3from
+4activity
+5group by user_id,action
+6having count(action_date)>=5
+7order by streak_length desc;
