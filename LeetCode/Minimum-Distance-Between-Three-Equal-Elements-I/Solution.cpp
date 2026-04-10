37b32afb3e9cc9951b96746438f08df4d7ef9c06
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-04-10 20:11:43 +0530

    Add solution for Minimum Distance Between Three Equal Elements I

--- LeetCode/Minimum-Distance-Between-Three-Equal-Elements-I/Solution.cpp
+++ LeetCode/Minimum-Distance-Between-Three-Equal-Elements-I/Solution.cpp
@@ -1,29 +1,8 @@
-class Solution {
-public:
-    int minimumDistance(vector<int>& nums) {
-        int Min=INT_MAX;
-        if(nums.size()<3)
-        {
-            return -1;
-        }
-        for(int i=0;i<nums.size()-2;i++)
-        {
-            for(int j=i+1;j<nums.size()-1;j++)
-            {
-                for(int k=j+1;k<nums.size();k++)
-                {
-                    if(nums[i]==nums[j] && nums[j]==nums[k] && nums[k]== nums[i])
-                    {
-                        int dis=(j-i)+(k-j)+(k-i);
-                        Min=min(Min,dis);
-                    }
-                }
-            }
-        }
-        if(Min==INT_MAX)
-        {
-            return -1;
-        }
-        return Min;
-    }
-};
+[1,1,1]
+[1,2,3,4,5]
+[2,2,2,2]
+[1,2,1,2,1]
+[1,1,2,2,1,1]
+[3,3,1,3,3]
+[1,2,3,1,2,3,1]
+[5,4,3,2,1,5,5]
