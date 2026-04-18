2c0bde4374b99dfb371b2bb56f71023e757710c7
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-04-18 09:32:51 +0530

    Add solution for Mirror Distance of an Integer

--- LeetCode/Mirror-Distance-of-an-Integer/Solution.cpp
+++ LeetCode/Mirror-Distance-of-an-Integer/Solution.cpp
@@ -1,1 +1,14 @@
-mirrorDistance = n => Math.abs(n - +[...`${n}`].reverse().join``);
+1class Solution {
+2public:
+3    int mirrorDistance(int n) {
+4        int t=n;
+5        int c=0,b=0;
+6        while(t!=0)
+7        {
+8            c=t%10;
+9            b=b*10+c;
+10            t/=10;
+11        }
+12        return abs(b-n);
+13    }
+14};
