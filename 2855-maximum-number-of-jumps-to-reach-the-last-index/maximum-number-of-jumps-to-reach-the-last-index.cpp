d8e077a8af2386e9bf893152ebc7395db077a79c
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-05-10 21:08:58 +0530

    Time: 11 ms (93.79%) | Memory: 68.3 MB (84.78%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        vector<int>dp(nums.size(),-1);
+        dp[0]=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(target>=abs(nums[j]-nums[i]) && dp[j]!=-1)
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[nums.size()-1];
+    }
+};
