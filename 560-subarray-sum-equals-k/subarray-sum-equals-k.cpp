2e26c393f20c5220c9a7ab76bdbf98c3cf9e819c
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-07-22 10:52:19 +0530

    Time: 63 ms (32.27%) | Memory: 47.6 MB (28.95%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int>m;
+        m[0]=1;
+        int ans=0;
+        int sum=0;
+        for(auto &i:nums)
+        {
+            sum+=i;
+            if(m.find(sum-k)!=m.end())
+            {
+                ans+=m[sum-k];
+            }
+            m[sum]++;
+        }
+        return ans;
+    }
+};
