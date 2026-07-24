38df1ebba2753ca1925a3b3c50bbda4b79e81172
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-07-24 23:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (39.31%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool fun(TreeNode* root,TreeNode* subRoot)
+    {
+        if(!root && !subRoot)
+        {
+            return true;
+        }
+        if((!root && subRoot) || (root && !subRoot))
+        {
+            return false;
+        }
+        if(root->val!=subRoot->val)
+        {
+            return false;
+        }
+        return (fun(root->left,subRoot->left) && fun(root->right,subRoot->right));
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root)
+        {
+            return false;
+        }
+        if(root->val==subRoot->val)
+        {
+            if(fun(root,subRoot))
+            {
+                return true;
+            }
+        }
+        return (isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot));
+    }
+};
