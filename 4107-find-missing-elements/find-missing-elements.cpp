c60c7ad0d18054a85b8dda218021054590e1a38e
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-08-04 19:54:49 +0530

    Time: 11 ms (6.06%) | Memory: 33.8 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int Min=INT_MAX,Max=0;
         set<int>s;
-        vector<int>v;
         for(auto &i:nums)
         {
-            Min=min(Min,i);
-            Max=max(Max,i);
             s.insert(i);
         }
+        int Min=*min_element(nums.begin(),nums.end());
+        int Max=*max_element(nums.begin(),nums.end());
+        vector<int>ans;
         for(int i=Min;i<=Max;i++)
         {
             if(s.find(i)==s.end())
             {
-                v.push_back(i);
+                ans.push_back(i);
             }
         }
-        return v;
+        return ans;
     }
 };
