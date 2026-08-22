f8c58334296912b608ee7d4633ab9e4cfde8db42
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-08-22 16:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.42%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        int t=n;
-        int sum=0,product=1;
+        int N=n;
+        int sum=0,pro=1;
         while(n!=0)
         {
-            sum+=n%10;
-            product*=n%10;
+            int temp=n%10;
+            sum+=temp;
+            pro*=temp;
             n/=10;
         }
-        cout <<sum <<" " <<product;
-        if(t%(sum+product))
-        {
-            return false;
-        }
-        return true;
+        return N%(sum+pro)==0;
     }
 };
